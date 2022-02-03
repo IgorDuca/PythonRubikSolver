698f6ce1dc6608700199f0abaaa5144c1e99b581
Igor Duca <igogithub@gmail.com>
2022-02-02 23:41:55 -0300

    Passing functions through external files

---
+++ recognize.py
@@ -0,0 +1,8 @@
+from pprint import pprint
+
+class Recognize:
+    def whitecross(blocks):
+        for face in blocks:
+            if face[1].startswith("W") and face[3].startswith("W") and face[4].startswith("W") and face[5].startswith("W") and face[7].startswith("W"):
+                pprint("Face {} is matching as a whitecross.".format(face))
+            else: pprint("Face {} doesn't match as a whitecross.".format(face))
